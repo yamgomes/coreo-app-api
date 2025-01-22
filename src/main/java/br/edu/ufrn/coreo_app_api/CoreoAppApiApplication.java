59d1983caba6fc157a5309e82f6fdb686a7b6718
Yam Gomes <yamgcunha@gmail.com>
2025-01-21 22:05:42 -0300

    projeto inicial

---
+++ src/main/java/br/edu/ufrn/coreo_app_api/CoreoAppApiApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ufrn.coreo_app_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreoAppApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreoAppApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ufrn/coreo_app_api/CoreoAppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ufrn.coreo_app_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreoAppApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
